f00fc190f2d343fef53bf98542d3aeefea64d629
Max <79068976350@yandex.ru>
2022-07-02 19:00:15 +0400

    add build.js

--- build.js
+++ build.js
@@ -1,27 +1,58 @@
+// https://habr.com/ru/company/ruvds/blog/424969/
 import * as fs from 'fs'
+import * as path from 'path'
 
 const getFiles = function (dir, recursive = true) {
-  const result = []
+  let result = []
+  const basename = path.basename(dir)
   const files = fs.readdirSync(dir)
   for (const i in files) {
-    const name = dir + '/' + files[i]
-    if (fs.statSync(name).isDirectory()) {
+    const filename = files[i]
+    const ext = path.extname(filename)
+    if (ext !== '.sql') {
+      continue
+    }
+    const filepath = dir + '/' + filename
+    if (fs.statSync(filepath).isDirectory()) {
       if (recursive) {
-        result.concat(getFiles(name))
+        result = result.concat(getFiles(filepath))
       }
     }
     else {
-      result.push(name)
+      if (path.basename(filename, ext) === basename) {
+        result.unshift(filepath)
+      }
+      else {
+        result.push(filepath)
+      }
     }
   }
   return result
 }
 
-// fs.readFile('data.json', (e, data) => {
-//   if (e) {
-//     throw e
-//   }
-//   console.log(data)
-// })
+const files = [
+  ...getFiles('./helpers'),
+  ...getFiles('./types', false),
+  ...getFiles('./types/set'),
+  ...getFiles('./types/constraint_def'),
+  ...getFiles('./rules'),
+  ...getFiles('./domains'),
+  ...getFiles('./validate'),
+]
 
-getFiles('./types')
+let content = ''
+for (const i in files) {
+  const filepath = files[i]
+  const data = fs.readFileSync(filepath, 'utf8')
+
+  const title = path.parse(filepath).name.toUpperCase()
+  const header =
+    `/*
+=================== ${title} =================== 
+*/\n`
+  content += header + data
+}
+
+const version = process.env.npm_package_version
+const filepath = `./dist/pg_validate--${version}.sql`
+fs.writeFileSync(filepath, content, 'utf8')
