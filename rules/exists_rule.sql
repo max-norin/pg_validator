0ab71dd1d7c672eb443db60f94a9480c2e84ac7e
Max <79068976350@yandex.ru>
2022-07-02 20:38:23 +0400

    optimize exists_rule.sql and refactor

--- rules/exists_rule.sql
+++ rules/exists_rule.sql
@@ -1,36 +1,41 @@
-CREATE OR REPLACE FUNCTION exists_rule("table" TEXT, "table_columns" TEXT[], "record" JSONB, "record_columns" TEXT[], "mode" FK_MODE = 'full',
-                                               "where" TEXT = NULL) RETURNS BOOLEAN AS
+CREATE FUNCTION exists_rule("table" TEXT, "table_columns" TEXT[], "record" JSONB, "record_columns" TEXT[], "mode" FK_MODE = 'full', "where" TEXT = NULL) RETURNS BOOLEAN AS
 $$
 DECLARE
-    "sql"          TEXT;
-    "column"       TEXT;
-    "null_counter" INT     = 0;
-    "length"       INT     = array_length("table_columns", 1);
-    "values"       TEXT[]  = '{}';
-    "result"       BOOLEAN = FALSE;
+    "has_null" CONSTANT BOOLEAN = ("record" ->> "record_columns"[1]) IS NULL;
+    "is_null"           BOOLEAN ;
+    "index"             INT ;
+    "length"   CONSTANT INT     = array_length("table_columns", 1);
+    "values"            TEXT[]  = '{}';
+    "sql"               TEXT;
+    "result"            BOOLEAN = FALSE;
 BEGIN
     IF ("length" IS NULL) OR ("length" <!> array_length("record_columns", 1)) THEN
         RETURN FALSE;
     END IF;
     -- get "values" array of escaped variables
-    FOREACH "column" IN ARRAY "record_columns"
+    "index" = 1;
+    WHILE "index" <= "length"
         LOOP
             -- checks only MATCH FULL can has NULL
-            IF "record" ->> "column" IS NULL THEN
+            "is_null" = "record" ->> "record_columns"["index"] IS NULL;
+            IF "is_null" THEN
                 IF "mode" != 'full' THEN
                     RETURN NULL;
                 ELSE
-                    -- done via counter because NULL can be at end
-                    "null_counter" = "null_counter" + 1;
+                    -- if first value is NULL and current value is NOT NULL, or vice versa
+                    IF "has_null" != "is_null" THEN
+                        RETURN FALSE;
+                    END IF;
                 END IF;
             ELSE
-                "values" = array_append("values", format('%L', "record" ->> "column"));
+                "values" = array_append("values", format('%L', "record" ->> "record_columns"["index"]));
             END IF;
+            "index" = "index" + 1;
         END LOOP;
     -- checks MATCH FULL
-    IF "null_counter" > 0 THEN
+    IF "has_null" THEN
         -- number of values is equal to number of NULL
-        RETURN "null_counter" = "length";
+        RETURN TRUE;
     END IF;
 
     "sql" = format('SELECT exists( SELECT * FROM %I WHERE (%s)=(%s) AND %s);', "table", array_to_string("table_columns", ','), array_to_string("values", ','), COALESCE("where", 'TRUE'));
